04c8a9496027ed1a7ac2ca2d53acca7f756637b2
michael0703 <liaoweiskewer0703@gmail.com>
2020-04-06 14:59:48 +0800

    Finish function code version1: pass the 1 egg test

--- test_1.py
+++ test_1.py
@@ -1,7 +1,10 @@
 import pytest
+import math
 
 def boilEgg(eggCount):
-	pass
+	#### one pot boils 8 eggs at most at the same time
+	needBoil = math.ceil(eggCount / 8)
+	return needBoil * 5
 
 class TestBoil:
 
